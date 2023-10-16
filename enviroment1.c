823c5303f8a35559dea37fe2c0b7ed9298bda1a0
99omniaashraf <omniaashraf249@gmail.com>
2023-10-16 09:24:03 -0700

    shell

--- enviroment1.c
+++ enviroment1.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * copy_info - copies info to create
- * a new env or alias
- * @name: name (env or alias)
- * @value: value (env or alias)
+ * copy_info - copies info to create.
+ * a new env or alias.
+ * @name: name (env or alias).
+ * @value: value (env or alias).
  *
  * Return: new env or alias.
  */
