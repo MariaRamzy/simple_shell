aa8ed81997d46c1ca9c70180d67db240d9074c81
99omniaashraf <omniaashraf249@gmail.com>
2023-10-18 13:20:43 -0700

    str2

--- aux_str2.c
+++ aux_str2.c
@@ -2,8 +2,8 @@
 
 /**
  * _strdup - duplicates a str in the heap memory.
- * @s: Type char pointer str
- * Return: duplicated str
+ * @s: Type char pointer str.
+ * Return: duplicated str.
  */
 char *_strdup(const char *s)
 {
